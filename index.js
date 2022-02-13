aa5781bfb236400d8fd1cdd84124329d06722eed
Alex Platt <alja.platt@gmail.com>
2022-02-13 09:58:26 +0000

    refactor js

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const deleteBtn = document.querySelector(".delete-btn");
 const taskList = document.querySelector(".task-list");
 const priceList = document.querySelector(".total-list");
 const total = document.querySelector(".total-amount");
-console.log(total);
-
-// const taskLi = document.createElement("li");
-// const priceLi = document.createElement("li");
-
-const mowPrice = 10;
-const washPrice = 15;
-const weedsPrice = 20;
 
 let taskArr = [];
 let priceArr = [];
@@ -33,19 +25,10 @@ const taskObj = {
   },
 };
 
-// console.log(taskObj);
-// console.log(taskObj[0].name);
-
-// empty array to store tasks
-// on click push value to empty array
-// loop through array and append items to list
-
 function renderTask() {
-  // move this here - DRY - clear out emojis before rendering
   taskList.innerHTML = "";
 
   for (let i = 0; i < taskArr.length; i++) {
-    // if(taskArr.includes)
     const taskItem = document.createElement("li");
     const deleteBtn = document.createElement("button");
     taskItem.classList.add("task-font");
@@ -67,7 +50,6 @@ function renderTotal() {
     taskPrice.textContent += `£${priceArr[i]}`;
     priceList.append(taskPrice);
     totalAmount += priceArr[i];
-    console.log(totalAmount);
   }
   total.textContent = `£${totalAmount}`;
 }
@@ -76,15 +58,9 @@ mowBtn.addEventListener("click", () => {
   if (!priceArr.includes(10)) {
     taskArr.push(taskObj[0].name); // 10
     priceArr.push(taskObj[0].price); // 10
-    console.log(taskArr);
-    console.log(priceArr);
 
     renderTask();
   }
-  // taskLi.textContent += "Mow Lawn";
-  // priceLi.textContent += `£ ${mowPrice}`;
-  // task.append(taskLi);
-  // price.append(priceLi);
 });
 
 washBtn.addEventListener("click", () => {
@@ -95,10 +71,6 @@ washBtn.addEventListener("click", () => {
     console.log(priceArr);
     renderTask();
   }
-  // taskLi.textContent = "Wash Car";
-  // priceLi.textContent += `£ ${washPrice}`;
-  // task.append(taskLi);
-  // price.append(priceLi);
 });
 
 weedsBtn.addEventListener("click", () => {
@@ -109,10 +81,6 @@ weedsBtn.addEventListener("click", () => {
     console.log(priceArr);
     renderTask();
   }
-  // taskLi.textContent = "Pull Weeds";
-  // priceLi.textContent += `£ ${weedsPrice}`;
-  // task.append(taskLi);
-  // price.append(priceLi);
 });
 
 invoiceBtn.addEventListener("click", () => {
@@ -121,10 +89,6 @@ invoiceBtn.addEventListener("click", () => {
   total.textContent = "£0";
   taskArr = [];
   priceArr = [];
-  // taskLi.textContent = "Pull Weeds";
-  // priceLi.textContent += `£ ${weedsPrice}`;
-  // task.append(taskLi);
-  // price.append(priceLi);
 });
 
 document.addEventListener("click", (e) => {
@@ -132,17 +96,10 @@ document.addEventListener("click", (e) => {
     e.target.className === "delete-btn" &&
     e.target.parentElement.textContent === "Mow Lawndelete"
   ) {
-    // console.log(e);
-    // console.log("delete me");
-    // console.log(taskArr.indexOf("Mow Lawn"));
-    // console.log(e.target.parentNode.textContent);
-    // console.log(e.target.parentElement.textContent);
     let index = taskArr.indexOf("Mow Lawn");
     let indexPrice = priceArr.indexOf(10);
-    console.log(index);
     taskArr.splice(index, 1);
     priceArr.splice(indexPrice, 1);
-    console.log(taskArr);
   }
   if (
     e.target.className === "delete-btn" &&
